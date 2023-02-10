a6746dc77546de76e6360cfbd924cf3a1b997b9f
tomtzook <tomtzook@gmail.com>
2023-02-10 13:10:51 +0200

    fix sim

--- src/main/java/frc/robot/sim/ElevatorSystemSim.java
+++ src/main/java/frc/robot/sim/ElevatorSystemSim.java
@@ -4,21 +4,22 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import frc.robot.Constants;
 
 public class ElevatorSystemSim {
 
     // those definitions are robot-specific
     // they are based on actual robot structure, sizes and such
     // please update them according to your actual robot
-    public static final double MOTOR_TO_WHEEL_GEAR_RATIO = 4;
+    public static final double MOTOR_TO_WHEEL_GEAR_RATIO = Constants.GEAR_RATIO_ELEVATOR;
     public static final double DRUM_RADIUS_M = Units.inchesToMeters(2.5);
     public static final double CARRIAGE_MASS_KG = 0.4;
-    public static final double MIN_HEIGHT_METERS = 0.5;
-    public static final double MAX_HEIGHT_METERS = 2;
-    public static final int MOTOR_COUNT = 2;
+    public static final double MIN_HEIGHT_METERS = 0;
+    public static final double MAX_HEIGHT_METERS = Constants.ELEVATOR_HEIGHT;
+    public static final int MOTOR_COUNT = 1;
     public static final DCMotor MOTORS_TYPE = DCMotor.getCIM(MOTOR_COUNT);
-    public static final boolean SIMULATE_GRAVITY = false;
-    private static final double HEIGHT_LIMIT_SWITCH_ACTIVE = MAX_HEIGHT_METERS - 0.05;
+    public static final boolean SIMULATE_GRAVITY = true;
+    private static final double HEIGHT_LIMIT_SWITCH_ACTIVE = 0.05;
 
     private final ElevatorSim sim;
     private double resetPos;
@@ -46,7 +47,7 @@ public class ElevatorSystemSim {
     }
 
     public boolean getLimitSwitch() {
-        return getHeightMeters() >= HEIGHT_LIMIT_SWITCH_ACTIVE;
+        return getHeightMeters() <= HEIGHT_LIMIT_SWITCH_ACTIVE;
     }
 
     public void update(double speed, double dt) {
