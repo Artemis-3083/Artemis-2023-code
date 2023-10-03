f179aa1e8df8238b5f90accc779f17c4987d98f9
FUFU42 <105926515+FUFU42@users.noreply.github.com>
2023-10-03 18:09:07 +0300

    Orgenized autonomuos

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -46,9 +46,7 @@ import frc.robot.commands.DriveBackwardScore;
 import frc.robot.commands.DriveBackwards;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.DriveForwardBalance;
-import frc.robot.commands.ElevatorDown;
 import frc.robot.commands.ElevatorPID;
-import frc.robot.commands.ElevatorUp;
 import frc.robot.commands.GripperPID;
 import frc.robot.commands.OpenCloseJoint;
 import frc.robot.commands.OpenFarJoint;
@@ -57,12 +55,10 @@ import frc.robot.commands.ResetArm;
 // import frc.robot.commands.ResetArmAndElevator;
 import frc.robot.commands.ResetDriveEncoders;
 import frc.robot.commands.ResetGripperEncoders;
-import frc.robot.commands.SetGripperEncoderTo1;
 import frc.robot.commands.ShootCube;
 import frc.robot.commands.ResetElevator;
 import frc.robot.commands.DriveForwardCommunity;
 // import frc.robot.commands.DriveUntilDistanceFromTag;
-import frc.robot.commands.Suck;
 // import frc.robot.commands.DriveUntilDistanceFromTag;
 import frc.robot.subsystems.DriveSystem;
 import frc.robot.subsystems.ElevatorSystem;
@@ -112,7 +108,6 @@ public class Robot extends TimedRobot {
     limelight = new LimelightSystem();
     camera = CameraServer.startAutomaticCapture();
     camera.setResolution(426, 240);
-    
 
     shootCube = new ShootCube(gripperSystem);
 
@@ -123,22 +118,22 @@ public class Robot extends TimedRobot {
     lowerArm = new ArmPID(11.038, 75.563, armSystem).alongWith(new ElevatorPID(-204, elevatorSystem));
 
 
-
     //auto chooser in Shuffleboard:
     autoChooser = new SendableChooser<>();
-    autoChooser.setDefaultOption("Default - Nothing", "resetEncoders");
-    autoChooser.addOption("Balance", "balance");
-    autoChooser.addOption("Community + balance", "communityAndBalance");
-    autoChooser.addOption("Score + balance", "scoreAndBalance");
-    autoChooser.addOption("Score", "score");
-    autoChooser.addOption("Score + community", "scoreAndCommunity");
-    autoChooser.addOption("Score + community + balance", "scoreAndCommunityAndBalance");
-    SmartDashboard.putData("Auto choosing", autoChooser);
 
-    armChooser = new SendableChooser<>();
-    armChooser.setDefaultOption("Default - with arm", "withArm");
-    armChooser.addOption("Without arm", "withoutArm");
-    SmartDashboard.putData("Arm choosing", armChooser);
+    autoChooser.setDefaultOption("Default - Nothing", "resetEncoders");
+
+    autoChooser.addOption("Score", "score");
+    autoChooser.addOption("Score + balance", "scoreAndBalance");
+    autoChooser.addOption("Score + community", "scoreAndCommunity");
+
+    autoChooser.addOption("Community + balance", "communityAndBalance");
+    autoChooser.addOption("Balance", "balance");
+    autoChooser.addOption("Community", "community");
+    
+    autoChooser.addOption("Score + community + balance (?)", "scoreAndCommunityAndBalance");
+
+    SmartDashboard.putData("Auto choosing", autoChooser);
 
 
     //Buttons:
@@ -155,11 +150,12 @@ public class Robot extends TimedRobot {
     new POVButton(controller, 90).toggleOnTrue(new OpenGripper(gripperSystem));
     new POVButton(controller, 270).toggleOnTrue(new CloseGripperAmp(gripperSystem));
     
+
     new JoystickButton(controller, PS4Controller.Button.kR1.value).whileTrue(new Collect(gripperSystem));
     new JoystickButton(controller, PS4Controller.Button.kL1.value).whileTrue(new Blow(gripperSystem));
 
     //Balance:
-    new JoystickButton(driveController, PS4Controller.Button.kSquare.value).whileTrue(new Balance(driveSystem));
+    new JoystickButton(driveController, PS4Controller.Button.kCircle.value).whileTrue(new Balance(driveSystem));
 
     //Drive:
     driveSystem.setDefaultCommand(new DriveCommand(driveSystem, driveController));
@@ -167,16 +163,6 @@ public class Robot extends TimedRobot {
  
   @Override
   public void robotPeriodic() {
-
-    // Input for Shuffleboard:
-    SmartDashboard.putNumber("Close joint encoder", armSystem.getCloseJoint());
-    SmartDashboard.putNumber("Far joint encoder", armSystem.getFarJoint());
-    SmartDashboard.putBoolean("Limit switch close joint", armSystem.getCloseSwitch());
-    SmartDashboard.putBoolean("Limit switch far joint", armSystem.getFarSwitch());
-    SmartDashboard.putNumber("Elevator hight", elevatorSystem.getHeight());
-    SmartDashboard.putNumber("Pitch", driveSystem.getPitch());
-    SmartDashboard.putNumber("Right front drive encoder", driveSystem.getDistancePassedRightM());
-    
     CommandScheduler.getInstance().run();
   }
   
@@ -203,73 +189,27 @@ public class Robot extends TimedRobot {
     Command communityState = new DriveBackwardScore(driveSystem).withTimeout(2).alongWith(armBalanceMode);
 
     //getting the auto from Shuffleboard:
-    String armSelected = armChooser.getSelected();
     String autoSelected = autoChooser.getSelected();
 
-    //
+    //starting the autoCommand iwth reseting the drive encoders
     Command autoCommand = new ResetDriveEncoders(driveSystem);
 
-    if(armSelected.equals("withArm")){
-      if(autoSelected.equals("balance")){
-        // autoCommand = balanceState1;
-        // autoCommand = autoCommand.andThen(autoBalanceState.withTimeout(3)).andThen(armBalanceMode.alongWith(new Balance(driveSystem)));
-        // autoCommand = autoCommand.andThen((resetCo048mmand.withTimeout(2).andThen(armBalanceMode)).alongWith(new DriveForwardBalance(driveSystem).andThen(new Balance(driveSystem))));
-      }else if(autoSelected.equals("communityAndBalance")){
-        // autoCommand = autoResetCommand;
-        // autoCommand = (.andThen(new DriveBackwards(driveSystem))).andThen(new DriveBackwards(driveSystem)).andThen(new Balance(driveSystem));
-        // autoCommand = autoCommand.andThen((autoResetCommand.withTimeout(2).andThen(armBalanceMode)).alongWith(new DriveForwardCommunity(driveSystem).andThen(new DriveBackwardScore(driveSystem)).andThen(new Balance(driveSystem))));
-      }else if(autoSelected.equals("scoreAndBalance")){
-        // autoCommand = scoreState3;
-        autoCommand = scoreState1.withTimeout(3).andThen(scoreState2).withTimeout(7).andThen(scoreState3);
-      }else if(autoSelected.equals("score")){
-        autoCommand = scoreState1.withTimeout(5).andThen(scoreState2);
-        // autoCommand = strightenArm.alongWith(new GripperPID(0.3, gripperSystem));//autoCommand.andThen(gripperState).alongWith(strightenArm);//.andThen(new ShootCube(gripperSystem).withTimeout(0.5)).withTimeout(3).andThen(resetCommand);
-      }else if(autoSelected.equals("scoreAndCommunity")){//dis community
-        autoCommand = new DriveForwardCommunity(driveSystem).andThen(communityState).withTimeout(5.5).andThen(new Balance(driveSystem));
-        // autoCommand = scoreState1.withTimeout(5).andThen(scoreState2).andThen(new DriveBackwardScore(driveSystem));
-      }else if(autoSelected.equals("scoreAndCommunityAndBalance")){
-  
-      }
-    }else if(armSelected.equals("withoutArm")){
-      if(autoSelected.equals("balance")){
-        autoCommand = autoCommand.andThen(new DriveForwardBalance(driveSystem)).andThen(new Balance(driveSystem));
-      }else if(autoSelected.equals("communityAndBalance")){
-        autoCommand = autoCommand.andThen(new DriveForwardCommunity(driveSystem)).andThen().andThen(new Balance(driveSystem));
-      }
+    
+    if(autoSelected.equals("score")){
+      autoCommand = autoCommand.andThen(scoreState1).withTimeout(4).andThen(new ShootCube(gripperSystem));
+    }else if(autoSelected.equals("scoreAndBalance")){
+      autoCommand = autoCommand.andThen(scoreState1).withTimeout(3).andThen(scoreState2).withTimeout(7).andThen(scoreState3);
+    }else if(autoSelected.equals("scoreAndCommunity")){
+      //scoreState1.withTimeout(5).andThen(scoreState2);
+      autoCommand = autoCommand.andThen(scoreState1).withTimeout(3).andThen(scoreState2).withTimeout(7).andThen(communityState);
+    }else if(autoSelected.equals("communityAndBalance")){
+      autoCommand = autoCommand.andThen(new DriveBackwardScore(driveSystem)).withTimeout(4).andThen(new DriveForwardCommunity(driveSystem)).withTimeout(2).andThen(new Balance(driveSystem)).alongWith(resetCommand.withTimeout(2).andThen(armBalanceMode));
+      //autoCommand = new DriveForwardCommunity(driveSystem).andThen(communityState).withTimeout(5.5).andThen(new Balance(driveSystem));
+    }else if(autoSelected.equals("community")){
+      autoCommand = autoCommand.andThen(new DriveBackwardScore(driveSystem)).withTimeout(3).alongWith(new ResetArm(armSystem));
     }
-    //Command farJoint = new ResetFarJoint(armSystem).andThen(new FarJointPID(5, armSystem));
-    //Command closeJoint = new ResetCloseJoint(armSystem).andThen(new CloseJointPID(10, armSystem));
-
-    // Command elevator = new ResetElevator(elevatorSystem).andThen(new ElevatorPID(-10, elevatorSystem));
-    // Command allPID = new LowerArm(armSystem).andThen(new ArmPID(90, 120, armSystem)).alongWith(elevator);
     
-    // new SetGripperEncoderTo1(gripperSystem).schedule();
     autoCommand.schedule();
-
-    //autonomus.schedule();
-
-    // new DriveForward(driveSystem).schedule();
-    //actual auto command
-    // resetCommand.withTimeout(3.5).andThen(new DriveForward(driveSystem)).andThen(new DriveBackwards(driveSystem)).andThen(new Balance(driveSystem)).schedule();
-    // new ResetDriveEncoders(driveSystem).schedule();
-
-
-    //new ResetDriveEncoders(driveSystem).andThen(new DriveForward(driveSystem)).andThen(new DriveBackwards(driveSystem)).andThen(new Balance(driveSystem)).schedule();
-    
-    
-    // resetCommand.schedule();
-      //elevator.schedule();
-    // new BalanceCommand(driveSystem).schedule();
-    // resetCommand.schedule();
-    // new DriveForward(driveSystem).schedule();
-    // new Balance(driveSystem).schedule();
-    //closeJoint.andThen(farjoint()).alongWith(elevator).schedule();
-    //new ResetElevator(elevatorSystem).andThen(new ElevatorPID(-10, elevatorSystem)).schedule();
-    //arm.alongWith(elevator).schedule();
-    //new DriveForward(driveSystem).withTimeout(1).andThen(new Balance(driveSystem)).schedule();
-    /*if (autoCommand != null) {
-      autoCommand.schedule();
-    }*/
   }
 
   @Override
@@ -286,8 +226,6 @@ public class Robot extends TimedRobot {
   
   @Override
   public void teleopInit() {
-    //new ElevatorDown(elevator).schedule();
-    //new ArmDown(arm).schedule();
   }
 
   @Override
